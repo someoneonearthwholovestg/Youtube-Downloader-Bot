8ae1cfaa8244ce1838e7849882b8cfcccc7ba628
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2021-01-02 07:25:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,7 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton(
-            "THANK ME", url="https://t.me/nabilanavab")]
+            "👑 THANK ME 👑", url="https://t.me/nabilanavab")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n\nTHIS IS A BOT THAT HELPS YOU TO CONVERT YOUTUBE LINKS TO VIDEO/FILES & MP3\n\n/help for More ifo"
     await message.reply_text(welcomed, reply_markup=joinButton)
