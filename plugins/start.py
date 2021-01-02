6a3598972b69f6d1147f822054f7a2d7f1c6aac3
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2021-01-02 07:09:56 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,9 +5,8 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "THANK ME", url="https://t.me/nabilanavab")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
